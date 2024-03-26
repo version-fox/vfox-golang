b0678ce84bdcac5ea0035132232d99542fef57eb
ShizheChang <35414447+ShizheChang@users.noreply.github.com>
2024-03-26 17:35:53 +0800

    Update the README and Reinitialize publish.yaml and metadata.lua from the template library. (#6)
    update info:
    1,Reinitialize publish.yaml and metadata.lua from the template library.
    2,Update the README information.
    3,Remove unnecessary comments.
    5,Delete outdated reference information.

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Golang plugin for vfox [vfox](https://vfox.lhan.me/) .
 
 ## Install
 After install vfox [vfox](https://vfox.lhan.me/),install the plugin by running:
-```vfox install nodejs```
+```vfox install golang```
--- metadata.lua
+++ metadata.lua
@@ -5,17 +5,42 @@ PLUGIN = {}
 --- Plugin name
 PLUGIN.name = "golang"
 --- Plugin author
-PLUGIN.author = "Aooohan"
+PLUGIN.author = "ShizheChang"
 --- Plugin version
-PLUGIN.version = "0.0.2"
---- Plugin repository
-PLUGIN.homepage = "https://github.com/ShizheChang/vfox-plugin-golang"
---- Plugin license
-PLUGIN.license = "MIT"
+PLUGIN.version = "0.1.0"
+--- Plugin homepage
+PLUGIN.homepage = "https://github.com/version-fox/vfox-golang"
+--- Plugin license, please choose a correct license according to your needs.
+PLUGIN.license = "Apache 2.0"
 --- Plugin description
 PLUGIN.description = ""
 
+
 --- !!! OPTIONAL !!!
+--[[
+NOTE:
+    Minimum compatible vfox version.
+    If the plugin is not compatible with the current vfox version,
+    vfox will not load the plugin and prompt the user to upgrade vfox.
+ --]]
+PLUGIN.minRuntimeVersion = "0.3.0"
+--[[
+NOTE:
+    If configured, vfox will check for updates to the plugin at this address,
+    otherwise it will check for updates at the global registry.
+
+    If you want use the global registry to distribute your plugin, you can remove this field.
+
+    If you develop a plugin based on the template, which will automatically generate a manifest file by CI,
+    you can set this address to the manifest file address, so that the plugin can be updated automatically.
+
+ --]]
+PLUGIN.manifestUrl = "https://github.com/version-fox/vfox-golang/releases/download/manifest/manifest.json"
+-- Some things that need user to be attention!
+PLUGIN.notes = {
+    "",
+}
+=======
 -- minimum compatible vfox version
 PLUGIN.minRuntimeVersion = "0.2.2"
 -- legacy filenames
