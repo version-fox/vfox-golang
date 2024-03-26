965477e75e5cccea055bee5e626c3837c77c4491
XvZheChuang <joneicu@163.com>
2024-03-26 09:41:08 +0800

    纠正一些lua语法错误

--- hooks/available.lua
+++ hooks/available.lua
@@ -1,5 +1,4 @@
-local util = require("util")
-
+require("utils")
 --- Return all available versions provided by this plugin
 --- @param ctx table Empty table used as context, for future extension
 --- @return table Descriptions of available versions and accompanying tool descriptions
