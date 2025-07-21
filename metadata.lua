4320c7d147aacd3ba959f1f22813efd20de0a2dc
Jiacheng <artin@cat.ms>
2025-07-21 19:38:45 +0800

    chore(release): release v0.4.0

--- metadata.lua
+++ metadata.lua
@@ -5,7 +5,7 @@ PLUGIN = {}
 --- Plugin name
 PLUGIN.name = "golang"
 --- Plugin version
-PLUGIN.version = "0.3.0"
+PLUGIN.version = "0.4.0"
 --- Plugin homepage
 PLUGIN.homepage = "https://github.com/version-fox/vfox-golang"
 --- Plugin license, please choose a correct license according to your needs.
